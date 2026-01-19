a2afe66e77ac567e36a2d2411efd8715c687b0f9
tr33 <me@tr33.de>
2026-01-19 22:57:54 +0100

    fixup

--- src/helpers/authentik.py
+++ src/helpers/authentik.py
@@ -26,7 +26,7 @@ def get_authentik_groups():
         groups_list = api_instance.core_groups_list(include_users=False).results
         for group in groups_list:
             if group_regex:
-                if not bool(group_regex.match(group.name)): continue
+                if not bool(group_regex.match(group.name, re.IGNORECASE)): continue
             authentik_groups.append(group.name)
 
     logger.info(f"Got {len(authentik_groups)} groups from Authentik")
